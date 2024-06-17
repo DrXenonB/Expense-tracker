35f3055224961d695cc904432aa5c966aa557bd7
Manjitha <manjithawaa@gmail.com>
2024-06-18 00:43:49 +0530

    Fixed empty list

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,17 @@ import ExpenseList from "./components/ExpenseList";
 import ExpenseFilter from "./components/ExpenseFilter";
 import ExpenseForm from "./components/ExpenseForm";
 
+interface ExpenseProps {
+  id: number;
+  description: string;
+  amount: number;
+  category: string;
+}
+
 function App() {
   const [categoryFilter, setCategoryFilter] = useState("");
 
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "", amount: 0, category: "" },
-  ]);
+  const [expenses, setExpenses] = useState<ExpenseProps[]>([]);
 
   const visibleCategory = categoryFilter
     ? expenses.filter((e) => e.category === categoryFilter)
@@ -17,23 +22,31 @@ function App() {
 
   return (
     <>
-      <div className="mb-5">
-        <ExpenseForm
-          onSubmit={(expense) =>
-            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
-          }
-        />
-      </div>
+      <div className="card mx-5 mt-4">
+        <div className="card-header text-center">Expense Tracker</div>
+        <div className="card-body">
+          <div className="mb-5">
+            <ExpenseForm
+              onSubmit={(expense) =>
+                setExpenses([
+                  ...expenses,
+                  { ...expense, id: expenses.length + 1 },
+                ])
+              }
+            />
+          </div>
 
-      <ExpenseFilter
-        onSelectCategory={(category) => setCategoryFilter(category)}
-      />
-      <ExpenseList
-        expenses={visibleCategory}
-        onDelete={(id) =>
-          setExpenses(expenses.filter((expense) => expense.id !== id))
-        }
-      />
+          <ExpenseFilter
+            onSelectCategory={(category) => setCategoryFilter(category)}
+          />
+          <ExpenseList
+            expenses={visibleCategory}
+            onDelete={(id) =>
+              setExpenses(expenses.filter((expense) => expense.id !== id))
+            }
+          />
+        </div>
+      </div>
     </>
   );
 }
